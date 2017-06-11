a9ee1267fcdca7d7a2ff69bc8e2026154ca78a09
ooztas <oztas@gmx.net>
2017-06-11 09:22:25 +0200

    complete SongItemService

--- src/app/SongView/services/son-item.service.ts
+++ src/app/SongView/services/son-item.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
+import {Headers, Http, RequestOptions, Response} from '@angular/http';
 import {SongItem} from '../../Model/songItem';
 import {Observable} from 'rxjs/Rx';
 
@@ -13,7 +13,7 @@ import 'rxjs/add/operator/catch';
 export class SonItemService {
 
   // Base Url
-  private baseUrl = 'http://#';
+  private baseUrl = 'http://#/v1/songs';
 
   // Resolve HTTP using the constructor
   constructor(private http: Http) {
@@ -22,19 +22,51 @@ export class SonItemService {
   // Get all songItems
   getSongs(): Observable<SongItem[]> {
     // Get request
-    return this.http.get(`${this.baseUrl}/songs`)
-      // and calling .json() on the response to return data
+    return this.http.get(`${this.baseUrl}`, this.getHeader())
+    // and calling .json() on the response to return data
       .map((res: Response) => res.json())
       // catch error
       .catch((error: any) => Observable.throw(error.json().error || 'get request server error'));
   }
 
   // Add a new songItem
+  addSong(body: Object): Observable<SongItem[]> {
+
+    return this.http.post(`${this.baseUrl}`, body, this.getHeader())
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'add a new songItem request server error'));
+  }
 
   // Get songItem by ID
+  getSongById(id: number): Observable<SongItem[]> {
+
+    return this.http.get(`${this.baseUrl}/${id}`, this.getHeader())
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'get SongItem by Id request server error'));
+  }
 
   // Update songItem by ID
+  updateSongById(id: number, body: Object): Observable<SongItem[]> {
+
+    return this.http.put(`${this.baseUrl}/${id}`, body, this.getHeader())
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'update songItem request error'));
+  }
 
   // Delete songItem by ID
+  deleteSongById(id: number): Observable<SongItem[]> {
+
+    return this.http.delete(`${this.baseUrl}/${id}`, this.getHeader())
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'delete a songItem request server error'));
+  }
+
+  // Private methods
+  private getHeader() {
+    const header = new Headers({'Content-Type': 'application/json'});
+    const options = new RequestOptions({headers: header});
+
+    return options;
+  }
 
 }
